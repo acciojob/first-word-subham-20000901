2b4445937d3c1eb404807a451e7e20fd793f05c7
subham-20000901 <sinhasubh2000@gmail.com>
2025-12-23 23:27:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	let str ="";
+	for(let i=0; i<s.length; i++) {
+		if(s[i]!== " "){
+			str += s[i];
+		}else break;
+	}
+	return str;
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
