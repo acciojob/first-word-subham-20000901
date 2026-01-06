f902c8168a2bc09819039338b633efccffaf91c5
subham-20000901 <sinhasubh2000@gmail.com>
2026-01-06 08:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function firstWord(s) {
-  // your code here
-	let str ="";
-	while(s[0] === " "){
-		s = s.slice(1);
-	}
-	
-	for(let i=0; i<s.length; i++) {
-		if(s[i]!== " "){
-			str += s[i];
-		}else break;
-	}
-	return str;
+if(s.length === 0) return "";
+s = s.trim();
+let str = "";
+for(let i=0; i<s.length; i++){
+	if(s[i] !== " "){
+		str += s[i];
+	}else break;
 }
-
+ return str;	
+}
 // // Do not change the code below
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
